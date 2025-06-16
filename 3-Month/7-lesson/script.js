7b380344022efbf94547800d73b127faecb65252
sardor0506 <eshmatovsardor0506@gmail.com>
2025-06-17 01:22:43 +0500

    update HTML and JS

--- 3-Month/7-lesson/script.js
+++ 3-Month/7-lesson/script.js
@@ -7,25 +7,25 @@ const elPassword = elForm.querySelector('.js-password')
 
 elForm.addEventListener('submit', (evt)=>{
     evt.preventDefault()
-    // try{
-    //     let username = elUsername.value;
-    //     let email = elEmail.value;
-    //     let password = elPassword.value;
+    try{
+        let username = elUsername.value;
+        let email = elEmail.value;
+        let password = elPassword.value;
 
-    //     if (!username) throw new Error("is not username !");
-    //     if (!email) throw new Error("is not Email !");
-    //     let isemail = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/
-    //     if (isemail.test(email)) throw new Error("The email value is incorrect");
-    //     if (!password) throw new Error("is not passeord !");
-    //     let user = {
-    //         elUsername: elUsername.value,
-    //         elEmail: elEmail.value,
-    //         elPassword: elPassword
-    //     }
-    //     console.log(user)
-    // }catch(err){
-    //     return alert(err.message)
-    // }
+        if (!username) throw new Error("is not username !");
+        if (!email) throw new Error("is not Email !");
+        let isemail = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/
+        if (isemail.test(email)) throw new Error("The email value is incorrect");
+        if (!password) throw new Error("is not passeord !");
+        let user = {
+            elUsername: elUsername.value,
+            elEmail: elEmail.value,
+            elPassword: elPassword
+        }
+        console.log(user)
+    }catch(err){
+        return alert(err.message)
+    }
         
     
 })
